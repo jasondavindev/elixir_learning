1e6ba5c99816e925bbbe9593b3bb15888912a7f6
jasondavindev <jasondavin.dev@gmail.com>
2020-12-03 22:57:01 -0300

    fix root file

--- lib/genetic_computing.ex
+++ lib/genetic_computing.ex
@@ -1,12 +1,18 @@
 defmodule GeneticComputing do
   alias Population
+
   def run do
-    pop = Population.generate_population(50)
+    pop = Population.generate_population(100)
 
     IO.puts("Best initial fitness #{Population.best_chromossome(pop).fitness}")
+    num_schedulers = :erlang.system_info(:schedulers_online)
+    parallel_evolution = Population.evolute(pop, 100_000, num_schedulers, :parallel)
+    IO.puts("Time spent: #{parallel_evolution.spent_time}")
 
-    pop = Population.evolute(pop, 100_000)
-
-    IO.puts("Best fitness after mutations #{Population.best_chromossome(pop).fitness}")
+    IO.puts(
+      "Best fitness after mutations #{
+        Population.best_chromossome(parallel_evolution.population).fitness
+      }"
+    )
   end
 end
